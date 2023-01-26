000e8031a985a5fedc535594e2e81c8a6f43aff5
muntasir-fahim <fa3877957@gmail.com>
2023-01-26 20:26:37 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Home from "./components/Home";
 
 import About from "./components/About";
 import NotFound from "./components/NotFound";
+import Gallery from "./components/Gallery";
+import Features from "./components/Features";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -11,6 +14,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
